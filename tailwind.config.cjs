9fc19a5a45bee15c6a87db422d8b21b0bd131c0a
Antonio Ibarrola <ntonio.ibarrola@gmail.com>
2022-12-10 21:03:41 -0800

    Initial "create-t3-app" setup

---
+++ tailwind.config.cjs
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('tailwindcss-fluid-type')({
+      settings: {
+        fontSizeMin: 0.75,
+        fontSizeMax: 1.25,
+        ratioMin: 1.125,
+        ratioMax: 1.2,
+        screenMin: 20,
+        screenMax: 96,
+        unit: 'rem',
+        prefix: 'fluid-',
+        extendValues: true,
+      },
+      values: {
+        xs: [-2, 1.6],
+        sm: [-1, 1.6],
+        base: [0, 1.6],
+        lg: [1, 1.6],
+        xl: [2, 1.2],
+        '2xl': [3, 1.2],
+        '3xl': [4, 1.2],
+        '4xl': [5, 1.1],
+        '5xl': [6, 1.1],
+        '6xl': [7, 1.1],
+        '7xl': [8, 1],
+        '8xl': [9, 1],
+        '9xl': [10, 1],
+      },
+    }),
+  ],
+};
